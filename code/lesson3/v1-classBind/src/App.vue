e5e163716af81f2e1a7a97e674faa65a02f42130
Helder Pereira <phelder@gmail.com>
2022-02-11 18:58:55 +0000

    Exercise

--- code/lesson3/v1-classBind/src/App.vue
+++ code/lesson3/v1-classBind/src/App.vue
@@ -1,15 +1,19 @@
 <template>
+  <ExerciseComponent />
+  <!-- <br>
   <Box class="yellowBorder" :showGreenText="false" />
   <br>
-  <Box class="yellowBorder" :showGreenText="true" />
+  <Box class="yellowBorder" :showGreenText="true" /> -->
 </template>
 
 <script>
 import Box from "./components/Box.vue";
+import ExerciseComponent from "./components/ExerciseComponent.vue";
 
 export default {
   components: {
-    Box
+    Box,
+    ExerciseComponent
   }
 }
 </script>
---
+++ code/lesson3/v1-classBind/src/components/ExerciseComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Exercise</h1>
+    <div class="colorDiv"></div>
+    <label>
+        Add a nice border: 
+        <input type="checkbox">
+    </label>
+</template>
+
+<style scoped>
+.colorDiv {
+    background-color: green;
+    width: 200px;
+    height: 200px;
+}
+</style>
