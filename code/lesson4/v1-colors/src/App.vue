05d81948e3e2ad762dbf580aa84b7f39c22ebaad
Helder Pereira <phelder@gmail.com>
2022-02-14 17:34:32 +0000

    exercise

---
+++ code/lesson4/v1-colors/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hi</h1>
+</template>
+
+<script>
+export default {
+// EXERCISE:
+// 1 - Create a new component Color component
+// 2 - that component should receive an array of colors as a prop. Ex: ["red", "green", "#000"]
+// 2.1 - For each element in the array create a circle 75px diameter
+// 2.2 - The circle should have the respective color
+// 3 - The component should display in a row a circle for each color in the prop
+// 4 - Odd circles should have a black border, Even circles should have a grey border
+// 5 - Show the name of the color inside the circle
+}
+</script>
